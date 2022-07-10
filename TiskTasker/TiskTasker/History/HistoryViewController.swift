fa549e5e3fee9e38735024eb1f47c745fa500a31
schlanjo <schlanjo@gmail.com>
2022-07-10 17:50:05 -0500

    Build initial storyboard framework for 3 MVP views

---
+++ TiskTasker/TiskTasker/History/HistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryViewController.swift
+//  TiskTasker
+//
+//  Created by Jon Schlandt on 7/6/22.
+//
+
+import UIKit
+
+class HistoryViewController: UITableViewController {
+    @IBAction func unwindLocationCancel(segue: UIStoryboardSegue) {}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ TiskTasker/TiskTasker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TiskTasker
+//
+//  Created by Jon Schlandt on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
