2e370a02b33da22d8fa6a479c2241c2ab91f343d
schlanjo <schlanjo@gmail.com>
2022-07-22 11:00:20 -0500

    Connect data model to Stats view

--- TiskTasker/TiskTasker/Stats/StatsViewController.swift
+++ TiskTasker/TiskTasker/Stats/StatsViewController.swift
@@ -8,14 +8,39 @@
 import UIKit
 
 class StatsViewController: UITableViewController {
+    @IBOutlet var statsTableView: StatsTableView!
     let manager = StatsDataManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         manager.fetch()
+        mapStatsToView()
     }
     
     private func initialize() {
         manager.fetch()
+    }
+    
+    private func mapStatsToView() {
+        if let stats = manager.taskStats {
+            mapInitialsToView(stats: stats)
+            statsTableView.nameLabel.text = stats.fullName
+            statsTableView.totalTasksLabel.text = stats.totalTasks?.description
+            statsTableView.totalPointsLabel.text = stats.totalPoints?.description
+        }
+    }
+    
+    private func mapInitialsToView(stats: TaskStats) {
+        var initials = ""
+        let names = stats.fullName?.components(separatedBy: " ")
+        
+        if let names = names {
+            for name in names {
+                initials += name[name.index(name.startIndex, offsetBy: 0)].uppercased()
+            }
+        }
+        
+        statsTableView.initialsLabel.text = initials
     }
 }
---
+++ TiskTasker/TiskTasker/Stats/View/StatsTableView.swift
@@ -0,0 +1,15 @@
+//
+//  StatsTableView.swift
+//  TiskTasker
+//
+//  Created by Jon Schlandt on 7/22/22.
+//
+
+import UIKit
+
+class StatsTableView: UITableView {
+    @IBOutlet var initialsLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var totalTasksLabel: UILabel!
+    @IBOutlet var totalPointsLabel: UILabel!
+}
